7afbbbb9bdc832b7e3bacb03cae18bdf49115056
Ibrahim756 <Ibrahim756@abv.bg>
2020-02-16 10:00:21 +0200

    axxa

---
+++ Solution2/05_word_4_Change/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_word_4_Change
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int  a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = 0;
+
+            Console.WriteLine("Before");
+            Console.WriteLine($"a={a}");
+            Console.WriteLine($"b={b}");
+
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("After");
+            Console.WriteLine($"a={a}");
+            Console.WriteLine($"b={b}");
+        }
+    }
+}
