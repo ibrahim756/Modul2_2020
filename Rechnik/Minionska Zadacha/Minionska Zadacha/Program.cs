fb45b2de58243aecee191e75a62446882d9b2a5b
Ibrahim756 <Ibrahim756@abv.bg>
2020-02-29 12:26:42 +0200

    minion

--- Rechnik/Minionska Zadacha/Minionska Zadacha/Program.cs
+++ Rechnik/Minionska Zadacha/Minionska Zadacha/Program.cs
@@ -10,6 +10,30 @@ namespace Minionska_Zadacha
     {
         static void Main(string[] args)
         {
+            Dictionary<string, int> mina = new Dictionary<string, int>();
+            while (true)
+            {
+                string izkopaemo = Console.ReadLine();
+                if (izkopaemo=="stop")
+                {
+                    break;
+                }
+
+                int kol = int.Parse(Console.ReadLine());
+                if (mina.ContainsKey(izkopaemo))
+                {
+                    mina[izkopaemo] += kol;
+                }
+                else
+                {
+                    mina.Add(izkopaemo, kol);
+                }
+
+            }
+            foreach (var item in mina)
+            {
+                Console.WriteLine($"{item.Key} => {item.Value}");
+            }
         }
     }
 }
